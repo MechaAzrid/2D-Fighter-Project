4d2dab80052591ed19c6cbbd434f82352a269699
Mark <1007004@student.sae.edu.au>
2018-12-15 21:05:38 +1100

    More animations

--- 2D Fighter/Assets/Scripts/PlayerController.cs
+++ 2D Fighter/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 
     private Animator Anim;
 
+    private Vector2 lastMove;
+
     void Start()
     {
         Anim = GetComponent<Animator>();
@@ -15,17 +17,31 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        //{
+        //    transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * Speed * Time.deltaTime, 0f, 0f));
+        //}
+
+        //if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        //{
+        //    transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * Speed * Time.deltaTime, 0f));
+        //}
+
+        //Anim.SetFloat("Move X", Input.GetAxisRaw("Horizontal"));
+        //Anim.SetFloat("Move Y", Input.GetAxisRaw("Vertical"));
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * Speed * Time.deltaTime, 0f, 0f));
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * Speed * Time.deltaTime, 0f));
+            lastMove = new Vector2(Input.GetAxisRaw("Vertical"), 0f);
         }
 
-        Anim.SetFloat("Move X", Input.GetAxisRaw("Horizontal"));
-        Anim.SetFloat("Move Y", Input.GetAxisRaw("Vertical"));
+        Anim.SetFloat("Last Move X", lastMove.x);
+        Anim.SetFloat("Last Move Y", lastMove.y);
     }
 }
